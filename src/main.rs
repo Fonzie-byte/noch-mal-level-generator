7d30916d11f4d670f13074c118e3b1168430f93b
Fons <40026023+Fonzie-byte@users.noreply.github.com>
2025-08-03 20:51:31 +0200

    feat: Very rudimentary idea of what it should look like
    I'd like to split this up in a main and lib, then I want to choose the colours randomly, then I want to write some check that checks if the level is valid.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,208 @@
+#[derive(Debug)]
+enum Color {
+    Green,
+    Yellow,
+    Cyan,
+    Pink,
+    Orange,
+}
+
+#[derive(Debug)]
+struct Column {
+    letter: char,
+    cells: [Color; 7],
+}
+
+#[derive(Debug)]
+struct Board {
+    columns: [Column; 15],
+}
+
 fn main() {
-    println!("Hello, world!");
+    let board = Board {
+        columns: [
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+            Column {
+                letter: 'A',
+                cells: [
+                    Color::Green,
+                    Color::Pink,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Cyan,
+                    Color::Orange,
+                    Color::Yellow,
+                ],
+            },
+        ],
+    };
+
+    dbg!(board);
 }
